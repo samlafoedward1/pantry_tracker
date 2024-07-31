91a82ae83d9645ad70c34c2d06cdc4c358e34d96
Edward Mawuko Samlafo-Adams <edwardmawuko75@gmail.com>
2024-07-31 22:29:02 +0200

    added boiler page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
-import React, { useState, useEffect } from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  Button,
+  Modal,
+  TextField,
+} from "@mui/material";
+import { Firestore } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore";
+import { transform } from "next/dist/build/swc";
+
+const style = {
+  postion: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%",
+  width: 400,
+  bgcolor: "white",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: 3,
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-center text-4xl p-4">Pantry Tracker</h1>
-      </div>
-    </main>
+    <Box>
+      <Typography variant="h1">Pantry Tracker</Typography>
+    </Box>
   );
 }
